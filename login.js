920774c83f7fbcd719a7d2c60d4a4e2efd2bc849
SARAH ONZETO SILVA <sarah.onzeto@alunoescolassociais.org.br>
2023-07-07 09:30:37 -0300

    so pra salvar

--- login.js
+++ login.js
@@ -27,7 +27,16 @@ function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome usuário:</th><th>Ações:</ th></tr>";
 
     for(let i=0; i<= (dadosLista.length -1); i++){
-        tabela += "<tr><td>" + dadosLista[i]  + "</td><td> <button onclick=''>Editar</button><button onclick=''>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i]  + "</td><td> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick=''>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela; 
     }
 } 
+//FUNÇÃO PARA EDIÇÃO DE NOME
+
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)]; 
+    dadosLista.splice(dadosLista[(i-1), 1]);
+}
+
+//FUNÇÃO PARA EXCLUIR NOME
+
