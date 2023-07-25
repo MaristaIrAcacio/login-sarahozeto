83df7f96c96c9d2baafdf93af75116a66aae4dcd
Sarah <ozeto.sarah.02@gmail.com>
2023-07-25 12:11:12 -0300

    adicionamos botão excluir

--- login.js
+++ login.js
@@ -20,6 +20,7 @@ function salvarUsuario(){
     if(nomeUsuario){
         dadosLista.push(nomeUsuario);
         criaLista();
+        document.getElementById(nomeUser).value = ''
         //console.log(dadosLista);
     }
 }
@@ -27,16 +28,22 @@ function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome usuário:</th><th>Ações:</ th></tr>";
 
     for(let i=0; i<= (dadosLista.length -1); i++){
-        tabela += "<tr><td>" + dadosLista[i]  + "</td><td> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick=''>Excluir</button></td></tr>";
+        tabela += "<tr><td>" + dadosLista[i]  + "</td><td> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela; 
     }
-} 
+}
+
 //FUNÇÃO PARA EDIÇÃO DE NOME
 
 function editar(i){
     document.getElementById("nomeUser").value = dadosLista[(i - 1)]; 
-    dadosLista.splice(dadosLista[(i-1), 1]);
+    dadosLista.splice(dadosLista[(i -1 )], 1);
 }
 
 //FUNÇÃO PARA EXCLUIR NOME
+
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById("tabela").deleteRow(i);
+}
 
